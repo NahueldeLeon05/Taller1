224f6f1333bbc6b4291fe8264c98368e811b1e31
nicoovillarr <nicolasvillar00@gmail.com>
2023-02-22 21:24:48 -0300

    Arreglar indentación y mejorar código de CargarBoolean.

--- Taller 1 2023/Boolean.cpp
+++ Taller 1 2023/Boolean.cpp
@@ -3,19 +3,16 @@
 
 //Carga un boolean
 void CargarBoolean(boolean &b){
-                int num;
-                printf("Ingrese un valor: 0-False 1-True:\n");
-                scanf("%d", &num);
-                if(num==1)
-                                b=TRUE;
-                else
-                                b=FALSE;
+    int num;
+    printf("Ingrese un valor: 0-False 1-True:\n");
+    scanf("%d", &num);
+    b = num == 1 ? TRUE : FALSE;
 }
 
 //muestra el boolean cargado
 void MostrarBoolean(boolean b){
-                if(b==TRUE)
-                                printf("Es Verdadero");
-                else
-                                printf("Es Falso");
+    if(b == TRUE)
+        printf("Es Verdadero");
+    else
+        printf("Es Falso");
 }
