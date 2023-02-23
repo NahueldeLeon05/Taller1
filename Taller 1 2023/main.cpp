7f246a9b5c1a54580c14ecd7f97a8427704362ae
nicoovillarr <nicolasvillar00@gmail.com>
2023-02-23 17:29:25 -0300

    Tests de fecha en main.

--- Taller 1 2023/main.cpp
+++ Taller 1 2023/main.cpp
@@ -1,4 +1,22 @@
+#include "Fecha.h"
+
 int main()
 {
+    String s;
+    while(true) {
+        printf("Ingresa una fecha: \r\n");
+        scan(s);
+        printf(ValidarFormato(s) ? "Formato es valido" : "Formato no es valido");
+        printf("\r\n");
+
+        Fecha f = TransformarFecha(s);
+        MostrarFecha(f);
+        printf("\r\n");
+
+        printf(ValidarFecha(f) ? "Fecha es valida" : "Fecha no es valida");
+        printf("\r\n");
+
+        LiberarString(s);
+    }
     return 0;
 }
